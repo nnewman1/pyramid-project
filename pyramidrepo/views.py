65814e0fb76f2c3e1a006dfff27764127f4fa18e
Nicholas Newman <nick.newman95@yahoo.com>
2017-02-23 11:56:36 -0700

    pyramid project

---
+++ pyramidrepo/views.py
@@ -0,0 +1,15 @@
+from pyramid.view import view_config
+import random
+
+def get_random_nickname():
+    adjvs = ['golden', 'ruby', 'fuzzy', 'liquid', 'sour', 'snowy', 'blazing']
+    nouns = ['shores', 'keys', 'peaks', 'smiles', 'jungles', 'skies', 'forests']
+
+    return random.choice(adjvs) + ' ' + random.choice(nouns)
+
+@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
+def my_view(request):
+    random_nickname = get_random_nickname()
+
+    return {'project': 'pyramid project', 'nickname': random_nickname}
+
